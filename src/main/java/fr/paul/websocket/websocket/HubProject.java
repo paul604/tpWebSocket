fbd3c9d376c0f904616b0ddb016d54b5ccd02bee
paul <paul604@users.noreply.github.com>
2017-11-30 09:43:13 +0100

    add methods

--- src/main/java/fr/paul/websocket/websocket/HubProject.java
+++ src/main/java/fr/paul/websocket/websocket/HubProject.java
@@ -1,12 +1,32 @@
 package fr.paul.websocket.websocket;
 
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
 /**
  * @author Paul
  */
-@ServerEndpoint("/")
+@ServerEndpoint("/hub")
 public class HubProject{
 
+    @OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println(message+" "+ session);
+    }
+
+    @OnError
+    public void onError(Session session, Throwable throwable){
+
+    }
+
+    @OnOpen
+    public void onOpen(Session session){
+
+    }
+
+    @OnClose
+    public void onClose(CloseReason reason, Session session){
+
+    }
 
 }
